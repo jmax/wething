1a3cc96fa0d0f54ed9a705e8b3009a310202914c
Esteban Soler <soleresteban@hotmail.com>
2013-06-05 18:17:26 -0300

    agregado spec y company

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Wething::Application.routes.draw do
   devise_for :users
 
-
+  get "home/index"
 
   root to: "home#index"
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130603195749) do
+ActiveRecord::Schema.define(version: 20130605163923) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -28,8 +34,10 @@ ActiveRecord::Schema.define(version: 20130603195749) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_id"
   end
 
+  add_index "users", ["company_id"], name: "index_users_on_company_id"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
